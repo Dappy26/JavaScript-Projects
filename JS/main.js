9327f526c3a1714958abf50d3af8889fdfc7c6d1
Dappy26 <makindedapo02@gmail.com>
2026-02-15 13:16:22 -0500

    Added JavaScipt project1

---
+++ JS/main.js
@@ -0,0 +1,6 @@
+let FirstName = "Dapo"; // declaring a variable and assigning it a value.
+let LastName = "Makinde"; // declaring another variable and assigning it a value.
+let message = "What's your name? " + ". My name is " + FirstName + " " + LastName + "."; // concatenating a string with the variable to create a message.
+window.alert(message); // Displaying the full sentence in an alert box
+document.write(message); // Displaying the full sentence on the webpage using document.write() method.
+
